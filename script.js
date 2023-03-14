65f76f2a0cf83808d65f661ab992af8231951f1f
AyomideDeborah <74296562+AyomideDeborah@users.noreply.github.com>
2023-03-14 02:56:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
 
-
-/*function changeIcon(img) {
-  img.src = 'images/icon-close.svg';
-}*/
-
-//function changeIcon(x) {
- // x.classList.toggle("images/icon-close.svg");
-//}
-
-
-
-/*function changeIcon() {
-  var img = document.getElementById('toggle').src;
-  if (img.indexOf('icon-hamburger.svg')!=-1) {
-    document.getElementById('toggle').src = 'images/icon-close.svg';
-  } else {
-    document.getElementById('toggle').src = 'images/icon-hamburger.svg';
-  }
-}*/
-
 function changeIcon() {
   var img = document.getElementById('toggle').src;
   var x = document.getElementById("nav-menu");
@@ -27,23 +7,15 @@ function changeIcon() {
     if (x.style.display === "none") {
       x.style.display = "block";
       
-    document.getElementById('toggle').src = 'images/icon-close.svg';
+    document.getElementById('toggle').src = 'icon-close.svg';
     } else {
     x.style.display = "none";
     
-    document.getElementById('toggle').src = 'images/icon-close.svg';
+    document.getElementById('toggle').src = 'icon-close.svg';
     } 
     
   } else {
-    document.getElementById('toggle').src = 'images/icon-hamburger.svg';
+    document.getElementById('toggle').src = 'icon-hamburger.svg';
   }
-  
-  /*var x = document.getElementById("nav-menu");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }*/
-  
 }
 
